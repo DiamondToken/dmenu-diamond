d56ee418f825f8b5e3e220936d58f4acaeb27788
DiamondToken <d.elizarov.00@gmail.com>
2022-03-31 20:20:04 +0300

    changed font

--- config.def.h
+++ config.def.h
@@ -5,13 +5,20 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Extended:size=20"
+	"Iosevka:size=22"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
+static const char col_gray1[] = "#002b36";
+static const char col_gray2[] = "#073642";
+static const char col_gray3[] = "#839496";
+static const char col_gray4[] = "#073642";
+static const char col_cyan[] = "#b58900";
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel] = { "#839496", "#073642"},
+	[SchemeNorm] = { col_gray3, col_gray1 },
+	[SchemeSel] = { col_gray4, col_cyan},
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
