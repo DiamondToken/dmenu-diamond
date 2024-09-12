7e38e6f343211bc24866a391d25eaa80c0e2e583
Elizarov Dmitry <d.elizarov.00@gmail.com>
2024-09-12 14:43:19 +0300

    gruvbox light.

--- config.def.h
+++ config.def.h
@@ -5,11 +5,11 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Nerd Font:size=14"
+	"Iosevka Nerd Font Mono:size=14"
 };
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
-static const char col_gray1[]       = "#282828";
+static const char col_gray1[]       = "#f9f5d7";
 static const char col_gray2[]       = "#504945";
 static const char col_gray3[]       = "#a89984";
 static const char col_gray4[]       = "#fbf1c7";
